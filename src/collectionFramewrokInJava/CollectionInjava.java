693ab9a581fe7589f54730d3d340af3cac0c3cbf
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-05-21 11:32:10 +0530

    Adding code

---
+++ src/collectionFramewrokInJava/CollectionInjava.java
@@ -0,0 +1,11 @@
+package collectionFramewrokInJava;
+
+public class CollectionInjava {
+	/**
+The Java collections framework (JCF) is a set of classes and interfaces that implement 
+commonly reusable collection data structures.
+Collection framework allow us to manipulate and store group of object.
+Collection Interface and classes are present in Java.utill package.
+	 */
+
+}
