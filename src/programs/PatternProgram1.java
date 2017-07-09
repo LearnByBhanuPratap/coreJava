229ee97f4513bff12e6d5dfbd38bc79e5bc55aa9
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-07-09 22:20:43 +0530

    add

--- src/programs/PatternProgram1.java
+++ src/programs/PatternProgram1.java
@@ -13,6 +13,11 @@ public class PatternProgram1 {
 
 	 */
 	public static void main(String[] args) {
-		
+		for(int i =1;i<=7;i++){
+			for(int j =1; j<=i;j++){
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
 	}
 }
--- src/programs/PatternProgram2.java
+++ src/programs/PatternProgram2.java
@@ -14,7 +14,7 @@ public class PatternProgram2 {
 	public static void main(String[] args) {
 		for(int i =1;i<=7;i++){
 			for(int j =1; j<=i;j++){
-				System.out.print("*"+" ");
+				System.out.print(i+" ");
 			}
 			System.out.println();
 		}
