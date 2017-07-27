31f4cf643237d021c368dde6d838a802820d745e
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-07-27 23:32:02 +0530

    add

--- src/programs/PatternProgram5.java
+++ src/programs/PatternProgram5.java
@@ -20,7 +20,7 @@ public class PatternProgram5 {
 		int number = scanner.nextInt();
 		for(int i=number;i>=1; i--){
 			for(int j = 1; j<=i;j++){
-				System.out.print(j+" ");
+				System.out.print("*"+" ");
 			}
 			System.out.println();
 		}
